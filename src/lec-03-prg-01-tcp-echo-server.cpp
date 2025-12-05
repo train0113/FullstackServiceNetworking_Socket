c71a5af25ce292de3b91fd11a153ed8da549b62d
train0113 <a01062338243@gmail.com>
2025-12-05 09:27:27 -0800

    tcp echo server/client

--- src/lec-03-prg-01-tcp-echo-server.cpp
+++ src/lec-03-prg-01-tcp-echo-server.cpp
@@ -1,5 +1,78 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
 #include <iostream>
+#include <string>
+
 int main() {
-    std::cout << "tcp echo server stub\\n";
+    const char* HOST = "127.0.0.1";
+    const int PORT = 65456;
+
+    std::cout << "> echo-server is activated\n";
+
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        std::perror("socket");
+        return 1;
+    }
+
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = inet_addr(HOST);
+
+    if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
+        std::perror("bind");
+        close(serverSocket);
+        return 1;
+    }
+
+    if (listen(serverSocket, 1) == -1) {
+        std::perror("listen");
+        close(serverSocket);
+        return 1;
+    }
+
+    sockaddr_in clientAddr{};
+    socklen_t clientLen = sizeof(clientAddr);
+    int clientSocket = accept(serverSocket, (sockaddr*)&clientAddr, &clientLen);
+    if (clientSocket == -1) {
+        std::perror("accept");
+        close(serverSocket);
+        return 1;
+    }
+
+    char clientIP[INET_ADDRSTRLEN] = {0};
+    inet_ntop(AF_INET, &clientAddr.sin_addr, clientIP, sizeof(clientIP));
+    int clientPort = ntohs(clientAddr.sin_port);
+
+    std::cout << "> client connected by IP address "
+              << clientIP << " with Port number " << clientPort << "\n";
+
+    const int BUF_SIZE = 1024;
+    char buffer[BUF_SIZE];
+
+    while (true) {
+        int recvLen = recv(clientSocket, buffer, BUF_SIZE, 0);
+        if (recvLen <= 0) {
+            break;
+        }
+
+        std::string msg(buffer, recvLen);
+        std::cout << "> echoed: " << msg << "\n";
+
+        send(clientSocket, msg.c_str(), msg.size(), 0);
+
+        if (msg == "quit") {
+            break;
+        }
+    }
+
+    close(clientSocket);
+    close(serverSocket);
+
+    std::cout << "> echo-server is de-activated\n";
     return 0;
 }
--- src/lec-03-prg-02-tcp-echo-client.cpp
+++ src/lec-03-prg-02-tcp-echo-client.cpp
@@ -1,5 +1,61 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
 #include <iostream>
+#include <string>
+
 int main() {
-    std::cout << "tcp echo server stub\\n";
+    const char* HOST = "127.0.0.1";
+    const int PORT = 65456;
+
+    std::cout << "> echo-client is activated\n";
+
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == -1) {
+        std::perror("socket");
+        return 1;
+    }
+
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = inet_addr(HOST);
+
+    if (connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
+        std::perror("connect");
+        close(clientSocket);
+        return 1;
+    }
+
+    const int BUF_SIZE = 1024;
+    char buffer[BUF_SIZE];
+
+    while (true) {
+        std::cout << "> ";
+        std::string sendMsg;
+        if (!std::getline(std::cin, sendMsg)) break;
+
+        if (send(clientSocket, sendMsg.c_str(), sendMsg.size(), 0) == -1) {
+            std::perror("send");
+            break;
+        }
+
+        int recvLen = recv(clientSocket, buffer, BUF_SIZE, 0);
+        if (recvLen <= 0) {
+            std::cout << "> server closed the connection\n";
+            break;
+        }
+
+        std::string recvMsg(buffer, recvLen);
+        std::cout << "> received: " << recvMsg << "\n";
+
+        if (sendMsg == "quit") break;
+    }
+
+    close(clientSocket);
+
+    std::cout << "> echo-client is de-activated\n";
     return 0;
 }
